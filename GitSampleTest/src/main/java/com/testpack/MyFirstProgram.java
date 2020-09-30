15663191996e95be08f02e8b3c9ebbeb1686e604
nsiva02 <welcome@DESKTOP-L2OG0MP>
2020-09-30 23:27:14 +0530

    added new code syso

--- GitSampleTest/src/main/java/com/testpack/MyFirstProgram.java
+++ GitSampleTest/src/main/java/com/testpack/MyFirstProgram.java
@@ -7,6 +7,10 @@ public class MyFirstProgram {
         System.out.println("Hi How are you");
         System.out.println("Hi i am fine");
         System.out.println("How do you do?");
+        System.out.println("I work in bangalore");
+        System.out.println("i hope you do well?");
+        System.out.println("I work in chennai");
+        
 
 	}
 
